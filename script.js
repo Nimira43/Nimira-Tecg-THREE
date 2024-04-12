5c2b29e6516a9fef701fbbb9a60995ea3b4be48d
Nick Rabone <n.rabone72@aol.com>
2024-04-12 16:27:38 +0100

    4th commit

--- script.js
+++ script.js
@@ -14,30 +14,36 @@ renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00FF00 })
-const mesh = new THREE.Mesh(boxGeometry, material)
-
-scene.add(mesh)
 camera.position.z = 5;
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
-const planeMaterial = new THREE.MeshBasicMaterial(
+const planeMaterial = new THREE.MeshPhongMaterial(
   {
     color: 0x0000ff,
-    side: THREE.DoubleSide
+    side: THREE.DoubleSide,
+    flatShading: THREE.FlatShading
   });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
-
 scene.add(planeMesh)
+
+console.log(planeMesh.geometry.attributes.position.array);
+
+const {array} = planeMesh.geometry.attributes.position
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i]
+  const y = array[i + 1]
+  const z = array[i + 2]
+  array[i + 2] = z + Math.random() 
+}
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(0, 0, 1)
+scene.add(light)
 
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera);
-  mesh.rotation.x += 0.01
-  mesh.rotation.y += 0.01
-  planeMesh.rotation.x += 0.03
-  planeMesh.rotation.y += 0.05
+  // planeMesh.rotation.y += 0.05
 }
 
 

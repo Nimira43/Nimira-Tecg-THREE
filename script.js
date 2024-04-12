5026cd15ce2cbd9f7cfe778d0c856684ce4df997
Nick Rabone <n.rabone72@aol.com>
2024-04-12 17:07:21 +0100

    5th - added dat.gui

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js"
+import * as dat from 'dat.gui'
+
+const gui = new dat.GUI()
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments: 10,
+    heightSegments: 10
+  }
+}
+
+gui.add(world.plane, 'width', 1, 20).onChange(generatePlane)
+gui.add(world.plane, "height", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "widthSegments", 1, 50).onChange(generatePlane);
+gui.add(world.plane, "heightSegments", 1, 50).onChange(generatePlane);
+
+function generatePlane() {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments,
+  );
+  const { array } = planeMesh.geometry.attributes.position;
+  for (let i = 0; i < array.length; i += 3) {
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
+    array[i + 2] = z + Math.random();
+  }
+}
 
 const scene = new THREE.Scene()
 const camera = new THREE.
@@ -13,7 +46,6 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
-
 camera.position.z = 5;
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
@@ -25,8 +57,6 @@ const planeMaterial = new THREE.MeshPhongMaterial(
   });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(planeMesh)
-
-console.log(planeMesh.geometry.attributes.position.array);
 
 const {array} = planeMesh.geometry.attributes.position
 for (let i = 0; i < array.length; i += 3) {
@@ -45,6 +75,5 @@ function animate() {
   renderer.render(scene, camera);
   // planeMesh.rotation.y += 0.05
 }
-
 
 animate()
